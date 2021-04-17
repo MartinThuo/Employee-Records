1abb440daeeb9c66bcc89749fac32f0775546cee
martin thuo <mertoenjosh3527@gmail.com>
2021-04-17 15:41:10 +0300

    done Employee cpp

--- Employee.cpp
+++ Employee.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 // Implementations for the Employee class methods
 namespace Records {
+
+    // Employee constructor sets initial values
     Employee::Employee()
     {
         mFirstName = "";
@@ -15,5 +17,81 @@ namespace Records {
         mEmployeeNumber = -1;
         mSalary = kDefaultStartingSalary;
         fHired = false;
+    }
+
+    void Employee::promote(int inRaiseAmount)
+    {
+        setSalary(getSalary() + inRaiseAmount);
+    }
+
+    void Employee::demote(int inDemeritAmount)
+    {
+        setSalary(getSalary() - inDemeritAmount);
+    }
+
+    void Employee::hire()
+    {
+        fHired = true;
+    }
+
+    void Employee::fire()
+    {
+        fHired = false;
+    }
+
+    void Employee::display()
+    {
+        cout << "Employee: " << getLastName() << ", " << getFirstName() << endl;
+        cout << "-------------------------" << endl;
+        cout << (fHired ? "Current Employee" : "Former Employee") << endl;
+        cout << "Employee Number: " << getEmployeeNumber() << endl;
+        cout << "Salary: $" << getSalary() << endl;
+        cout << endl;
+    }
+
+    // Accessors and setters 
+    void Employee::setFirstName(string inFirstName)
+    {
+        mFirstName = inFirstName;
+    }
+
+    string Employee::getFirstName()
+    {
+        return mFirstName;
+    }
+
+    void Employee::setLastName(string inLastName)
+    {
+        mLastName = inLastName;
+    }
+
+    string Employee::getLastName()
+    {
+        return mLastName;
+    }
+
+    void Employee::setEmployeeNumber(int inEmployeeNumber)
+    {
+        mEmployeeNumber = inEmployeeNumber;
+    }
+
+    int Employee::getEmployeeNumber()
+    {
+        return mEmployeeNumber;
+    }
+
+    void Employee::setSalary(int inSalary)
+    {
+        mSalary = inSalary;
+    }
+
+    int Employee::getSalary()
+    {
+        return mSalary;
+    }
+
+    bool Employee::getIsHired()
+    {
+        return fHired;
     }
 }
