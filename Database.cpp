b40b1c1c4436ea41cbea37e3c1917af5784e4e37
martin thuo <mertoenjosh3527@gmail.com>
2021-04-18 12:00:47 +0300

    database cpp ✅

--- Database.cpp
+++ Database.cpp
@@ -34,4 +34,54 @@ namespace Records {
 
         return theEmployee;
     }
+
+    Employee& Database::getEmployee(int inEmployeeNumber)
+    {
+        for (int i = 0; i < mNextSlot; i++) {
+            if (mEmployees[i].getEmployeeNumber() == inEmployeeNumber) {
+                return mEmployees[i];
+            }
+        }
+
+        cerr << "No employee with employee number " << inEmployeeNumber << endl;
+        throw exception();
+    }
+
+    Employee& Database::getEmployee(string inFirstName, string inLastName)
+    {
+        for (int i = 0; i < mNextSlot; i++) {
+            if (mEmployees[i].getFirstName() == inFirstName &&
+                mEmployees[i].getLastName() == inLastName) {
+                return mEmployees[i];
+            }
+        }
+
+        cerr << "No match with name " << inFirstName << " " << inLastName << endl;
+        throw exception();
+    }
+
+    void Database::displayAll()
+    {
+        for (int i = 0; i < mNextSlot; i++) {
+            mEmployees[i].display();
+        }
+    }
+
+    void Database::displayCurrent()
+    {
+        for (int i = 0; i < mNextSlot; i++) {
+            if (mEmployees[i].getIsHired()) {
+                mEmployees[i].display();
+            }
+        }
+    }
+
+    void Database::displayFormer()
+    {
+        for (int i = 0; i < mNextSlot; i++) {
+            if (!mEmployees[i].getIsHired()) {
+                mEmployees[i].display();
+            }
+        }
+    }
 }
